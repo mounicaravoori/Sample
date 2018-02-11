75d086b652fbb25330d6ac21fb7250d3699bf274
mounica.ravoori@b507.us <1028skP@>
2018-02-10 20:20:00 -0500

    added abstractcar java class to sample repo

---
+++ AbstractCar.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public abstract class AbstractCar {
+	void carBreaks() {
+		System.out.println("the car breaks are in perfect condition");
+	}
+	void carEnginee() {
+		System.out.println("Cars enginee is in perfect condition");
+	}
+	abstract void carInterior();
+	abstract void carExterior();
+
+
+
+
+}
+
+
